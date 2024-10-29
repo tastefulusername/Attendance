689ee885c1a81f477b59f5cd3e7d0128e9153e28
Nick <bobzilla525@gmail.com>
2024-10-28 18:26:46 -0600

    Add files via upload

---
+++ attendance/food.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# canvas_username canvas_password "the_class_name_on_iclicker" --latitude 0.0 --longitude 0.0
+python3 bot.py nrkorte Complication2001! "Food and Nutrition in Health" --latitude 40.5691235114163 --longitude -105.08530410921091
---
+++ attendance/soc.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# canvas_username canvas_password "the_class_name_on_iclicker" --latitude 0.0 --longitude 0.0
+python3 bot.py nrkorte Complication2001! "SOC100-002: Introduction to Sociology" --latitude 40.57137092489508 --longitude -105.08269497809158
